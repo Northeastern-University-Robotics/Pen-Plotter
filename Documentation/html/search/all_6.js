1e5ec64fbe63f1e172687f11ec0c11f11c3335f5
jaredcohennu <cohen.jar@northeastern.edu>
2021-10-30 23:14:00 -0900

    Added documentation for the MoteusException.py file

--- Documentation/html/search/all_6.js
+++ Documentation/html/search/all_6.js
@@ -1,4 +1,9 @@
 var searchData=
 [
-  ['pen_2dplotter',['Pen-Plotter',['../md_README.html',1,'']]]
+  ['message',['message',['../classMoteusException_1_1MoteusCanError.html#afd7945c7a219a2584b1a0f0630c174b9',1,'MoteusException::MoteusCanError']]],
+  ['moteus',['Moteus',['../classMoteus_1_1Moteus.html',1,'Moteus']]],
+  ['moteuscanerror',['MoteusCanError',['../classMoteusException_1_1MoteusCanError.html',1,'MoteusException']]],
+  ['moteusexception',['MoteusException',['../classMoteusException_1_1MoteusException.html',1,'MoteusException']]],
+  ['moteuspermissionserror',['MoteusPermissionsError',['../classMoteusException_1_1MoteusPermissionsError.html',1,'MoteusException']]],
+  ['moteuswarning',['MoteusWarning',['../classMoteusException_1_1MoteusWarning.html',1,'MoteusException']]]
 ];
---
+++ Documentation/html/search/all_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['setattributes',['setAttributes',['../classMoteus_1_1Moteus.html#a75698a813e2463f3c245a4b7fa07b2a0',1,'Moteus::Moteus']]],
+  ['setsimulationprinting',['setSimulationPrinting',['../classMoteusException_1_1MoteusWarning.html#af68079f6f69c3ea6d96d35104493621b',1,'MoteusException::MoteusWarning']]]
+];
