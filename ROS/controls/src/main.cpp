92f87eb07fee8e560c33b8e54139eea7e37c722d
Maxwell Pirtle <maxwellpirtle@me.com>
2021-11-10 17:19:42 -0500

    Modify C++ section of CMakeLists.txt

--- ROS/controls/src/main.cpp
+++ ROS/controls/src/main.cpp
@@ -1,5 +1,5 @@
-#include "nuc_motor_controls.h"
-
-int main(int argc, const char **argv) {
-    return 0;
-}
+//#include "nuc_motor_controls.h"
+//
+//int main(int argc, const char **argv) {
+//    return 0;
+//}
---
+++ ROS/controls/src/nuc_motor_controls.cpp
@@ -0,0 +1,5 @@
+#include "nuc_motor_controls.h"
+
+int main(int argc, const char **argv) {
+    return 0;
+}
